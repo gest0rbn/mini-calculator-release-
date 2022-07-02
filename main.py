e824dedb66b85e39908f3e05fe4962715ea568fd
gest0rbn <rafailgraph@gmail.com>
2022-07-02 22:16:33 +0300

    main

---
+++ main.py
@@ -0,0 +1,18 @@
+def calculate(s):
+    if '+' in s or '-' in s or '*' in s or '/' in s:
+        if '+' in s:
+            return int(s[:s.find('+')]) + int(s[s.find('+') + 1:])
+        elif '-' in s:
+            return int(s[:s.find('-')]) - int(s[s.find('-') + 1:])
+        elif '*' in s:
+            return int(s[:s.find('*')]) * int(s[s.find('*') + 1:])
+        else:
+            return int(s[:s.find('/')]) / int(s[s.find('/') + 1:])
+    else:
+        return 'You need to sleep. You was told, that you need to use above mentioned characters by me.'
+
+
+print('Enter your expression using "+", "-", "*" or "/".\n')
+st = input()
+
+print(calculate(st))
